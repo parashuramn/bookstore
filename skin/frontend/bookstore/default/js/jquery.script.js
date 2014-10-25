f78f557b6cb4700fafc13b7b537071142b9dd2bd
Mr Khoa <mkhoa99@gmail.com>
2014-10-26 00:15:16 +0700

    Product List and View continue.

--- skin/frontend/bookstore/default/js/jquery.script.js
+++ skin/frontend/bookstore/default/js/jquery.script.js
@@ -1,7 +1,7 @@
 /**
  * Created by Mr Khoa on 18/10/2014.
  */
-$j = jQuery.noConflict();
+var $j = jQuery.noConflict();
 //Our Custom Javascript
 $j(document).ready(function(){
     $j('.navbar .parent').addClass('dropdown');
@@ -43,6 +43,78 @@ $j(document).ready(function(){
             setHeightToMax(el.childElements());
         });
     //End Function Get Height automatically
+    /*Product Collateral Tab*/
+    // ==============================================
+    // UI Pattern - Toggle Content (tabs and accordions in one setup)
+    // ==============================================
 
+    $j('.toggle-content').each(function () {
+        var wrapper = jQuery(this);
+
+        var hasTabs = wrapper.hasClass('tabs');
+        var hasAccordion = wrapper.hasClass('accordion');
+        var startOpen = wrapper.hasClass('open');
+
+        var dl = wrapper.children('dl:first');
+        var dts = dl.children('dt');
+        var panes = dl.children('dd');
+        var groups = new Array(dts, panes);
+
+        //Create a ul for tabs if necessary.
+        if (hasTabs) {
+            var ul = jQuery('<ul class="toggle-tabs"></ul>');
+            dts.each(function () {
+                var dt = jQuery(this);
+                var li = jQuery('<li></li>');
+                li.html(dt.html());
+                ul.append(li);
+            });
+            ul.insertBefore(dl);
+            var lis = ul.children();
+            groups.push(lis);
+        }
+
+        //Add "last" classes.
+        var i;
+        for (i = 0; i < groups.length; i++) {
+            groups[i].filter(':last').addClass('last');
+        }
+
+        function toggleClasses(clickedItem, group) {
+            var index = group.index(clickedItem);
+            var i;
+            for (i = 0; i < groups.length; i++) {
+                groups[i].removeClass('current');
+                groups[i].eq(index).addClass('current');
+            }
+        }
+
+        //Toggle on tab (dt) click.
+        dts.on('click', function (e) {
+            //They clicked the current dt to close it. Restore the wrapper to unclicked state.
+            if (jQuery(this).hasClass('current') && wrapper.hasClass('accordion-open')) {
+                wrapper.removeClass('accordion-open');
+            } else {
+                //They're clicking something new. Reflect the explicit user interaction.
+                wrapper.addClass('accordion-open');
+            }
+            toggleClasses(jQuery(this), dts);
+        });
+
+        //Toggle on tab (li) click.
+        if (hasTabs) {
+            lis.on('click', function (e) {
+                toggleClasses(jQuery(this), lis);
+            });
+            //Open the first tab.
+            lis.eq(0).trigger('click');
+        }
+
+        //Open the first accordion if desired.
+        if (startOpen) {
+            dts.eq(0).trigger('click');
+        }
+
+    });
 
 });
